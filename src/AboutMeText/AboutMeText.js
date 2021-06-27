8294465fdd448d60c80ac21ed6d49c60c6130b87
Anna Maria <annamaria@Annas-MacBook-Pro.local>
2021-06-27 14:25:15 +0100

    image alt added

--- src/AboutMeText/AboutMeText.js
+++ src/AboutMeText/AboutMeText.js
@@ -5,7 +5,7 @@ export const AboutMeText = () => {
     return (
         <div className="centered_section">
             <h1 className="about_me">I'm Anna, a software development engineer</h1>
-            <img src={anna} width="200" height="200" />
+            <img src={anna} width="200" height="200" alt="anna logo" />
         </div>
     )
 }
